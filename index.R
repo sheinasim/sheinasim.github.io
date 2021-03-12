c013e98a5aec54c9b3fab8d0c74ed6f867d9ee57
sheinasim <ssim8@hawaii.edu>
2021-03-12 11:22:07 -1000

    Update: changed date format

--- index.R
+++ index.R
@@ -1,4 +1,4 @@
-## ----setup, include = F--------------------------------------------------------------------------------------------------------------------------------------------------
+## ----setup, include = F-------------------------------------------------------------------------------------------------------------
 
 library(tidyverse)
 library(plyr)
@@ -7,6 +7,7 @@ library(dplyr)
 library(ggplot2)
 library(readxl)
 library(flexdashboard)
+library(wesanderson)
 
 data_summary <- function(data, varname, groupnames){
   require(plyr)
@@ -20,30 +21,38 @@ data_summary <- function(data, varname, groupnames){
  return(data_sum)
 }
 
-
-## ----Read in file--------------------------------------------------------------------------------------------------------------------------------------------------------
-
-data_df <- read_tsv("test.txt", col_names=T, trim_ws = TRUE, skip_empty_rows = TRUE)
-
+pal1 <- wes_palette("Darjeeling1", 5, type = "continuous")
+pal2 <- wes_palette("FantasticFox1", 5, type = "continuous")
+pal3 <- wes_palette("Cavalcanti1", 5, type = "continuous")
 
 
-## ----plot line-----------------------------------------------------------------------------------------------------------------------------------------------------------
-ggplot(data=data_df, aes(x=X, y=Y1)) +
-  geom_line()+
-  geom_point()
+## ----Read in file-------------------------------------------------------------------------------------------------------------------
 
+data_xl_df <- readxl::read_xlsx("qlb_colony_spreadsheet.xlsx", col_names=T, sheet="lifestages") %>% 
+  gather(key = "Life stage", value = "Count", -Date) %>% 
+  mutate(`Life stage` = fct_relevel(`Life stage`, "Larvae", "Pupae", "Adult female", "Adult male", "Adult mating"))
 
-
-## ----plot hist-----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-ggplot(data_df, aes(x=Y1)) + geom_histogram()
+# View(data_xl_df)
 
 
 
-## ----plot lolli----------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----plot stackedbar----------------------------------------------------------------------------------------------------------------
 
-ggplot(data_df, aes(x=X, y=Y2)) +
-  geom_point() + 
-  geom_segment( aes(x=X, xend=X, y=0, yend=Y2))
+ggplot(data_xl_df, aes(fill=`Life stage`, y=Count, x=Date)) + 
+    geom_bar(position="stack", stat="identity") +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),) +
+    xlab("") +
+    scale_y_continuous(labels = scales::number_format(accuracy = 1)) + 
+    scale_fill_manual(values=pal3)
+    
+
+
+
+## ----plot lolli, echo=F-------------------------------------------------------------------------------------------------------------
+
+# ggplot(data_df, aes(x=X, y=Y2)) +
+#   geom_point() + 
+#   geom_segment( aes(x=X, xend=X, y=0, yend=Y2))
 
 
